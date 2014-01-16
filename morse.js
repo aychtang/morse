26dc8df2c6385f13953af9bbe48d9edb0b643226
Howard Tang <tangh@google.com>
2014-01-16 14:21:37 +0000

    Adds first pass english -> morse.

---
+++ morse.js
@@ -0,0 +1,56 @@
+
+var morse = function(letter) {
+	if (letter === ' ') return '       ';
+	var codes = {
+		'a' : [".", "-"],
+		'b' : ["-", ".", ".", "."],
+		'c' : ["-", ".", "-", "."],
+		'd' : ["-", ".", "."],
+		'e' : ["."],
+		'f' : [".", ".", "-", "."],
+		'g' : ["-", "-", "."],
+		'h' : ['.','.','.','.'],
+		'i' : ['.','.'],
+		'j' : ['.','-','-','-'],
+		'k' : ['-','.','-'],
+		'l' : ['.','-','.','.'],
+		'n' : ['-','.'],
+		'm' : ['-','-'],
+		'o' : ['-','-','-'],
+		'p' : ['.','-','-','.'],
+		'q' : ['-','-','.','-'],
+		'r' : ['.', '-','.'],
+		's' : ['.','.','.'],
+		't' : ['-'],
+		'u' : ['.','.','-'],
+		'v' : ['.','.','.','-'],
+		'w' : ['.','-','-'],
+		'x' : ['-','.','.','-'],
+		'y' : ['-','.','-','-'],
+		'z' : ['-','-','.','.']
+	};
+
+	return codes[letter].join(' ');
+};
+
+var mapStr = function(str, fn) {
+	var result = '';
+	for (var i = 0; i < str.length; i++) {
+		result += fn(str[i]);
+	}
+	return result;
+};
+
+var sentanceBuilder = function(fn) {
+	return function(i) {
+		return fn(i) + '   ';
+	};
+};
+
+var trimRight = function(str) {
+	return str.trim();
+};
+
+var morsify = function(str) {
+	return trimRight(mapStr(str, sentanceBuilder(morse)));
+};
